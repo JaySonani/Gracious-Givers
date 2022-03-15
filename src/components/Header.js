1d9c4aa7d0ee68df46921d6f341934347ec5b1eb
Akanksha Singh <ak302575@dal.ca>
2022-03-15 19:37:21 -0300

    Adding responsiveness to Header Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,30 +5,33 @@ import Label from './Label';
 
 import './styles/Header.css';
 
-const Header = () => {
-
+function Header() {
+  
   return (
-    <Navbar expand='lg' className='headerNav'>
-      <Container>
-        <Navbar.Brand href='/'>
-          <img src={BrandLogo}
-            width='110rem'
-            height='55rem'
-            alt='Gracious Givers' className='rounded' />
-        </Navbar.Brand>
-        <Nav className="me-auto">
-          <Label title="Home" path="/" />
-          <Label title="Fundraiser" path="/fundraiser" />
-          <Label title="About Us" path="/about_us" />
-        </Nav>
-        <Nav>
-          <Button variant="primary" className="custom-btn-header">
-            Login
-          </Button>
-        </Nav>
-      </Container>
-    </Navbar>
-  );
+      <Navbar expand='lg' className='headerNav' collapseOnSelect bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href='/home'>
+            <img src={BrandLogo}
+                width='110rem'
+                height='55rem' 
+                alt='Helping Hands' className='rounded' />
+          </Navbar.Brand> 
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto"> 
+              <Label title="Home" path="/" />
+              <Label title="Fundraiser" path="/fundraiser" />
+              <Label title="About Us" path="/about_us" />
+            </Nav>
+            <Nav>
+              <Button variant="primary" className="custom-btn-header">
+                  Login
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
 }
 
 export default Header;
