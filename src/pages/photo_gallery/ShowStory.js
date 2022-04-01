ab2a1443a62aa14cd5527884e6360dbf2b73bae1
viraj <viraj.shah@dal.ca>
2022-04-01 13:29:10 -0300

    ShowStory component added

---
+++ src/pages/photo_gallery/ShowStory.js
@@ -0,0 +1,15 @@
+import Footer from "../../components/navbar/Footer";
+import Header from "../../components/navbar/Header";
+import ShowStoryComponent from "../../components/photo_gallery/ShowStoryComponent";
+
+const ShowStoryPage = () => {
+    return (
+        <div>
+            <Header />
+            <ShowStoryComponent />
+            <Footer />
+        </div>
+    );
+}
+
+export default ShowStoryPage;
