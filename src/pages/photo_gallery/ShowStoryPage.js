e00f1d5f9d9e682dee5cd3b94ad6b61a068ea1d9
viraj <viraj.shah@dal.ca>
2022-04-04 03:00:22 -0300

    Show user Story component added

--- src/pages/photo_gallery/ShowStoryPage.js
+++ src/pages/photo_gallery/ShowStoryPage.js
@@ -4,14 +4,16 @@ import Footer from "../../components/navbar/Footer";
 import Header from "../../components/navbar/Header";
 import ShowStoryComponent from "../../components/photo_gallery/ShowStoryComponent";
 import { useNavigate } from "react-router-dom";
+import * as StoryConstant from "../../components/photo_gallery/StoryConstant"
 
 const ShowStoryPage = () => {
 
+    const ngoAuth = StoryConstant.getNgoId();
     const navigate = useNavigate();
     return (
         <div>
             <Header />
-            <ShowStoryComponent />
+            {ngoAuth.length > 0 ? <ShowStoryComponent /> : <ShowStoryUserComponent />}
             <Footer />
         </div>
     );
