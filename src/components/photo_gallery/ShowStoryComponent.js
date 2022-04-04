bacd900e03a3814429d7b50f43a573d7fd31d53c
viraj <viraj.shah@dal.ca>
2022-04-04 17:04:12 -0300

    styling added for show story component

--- src/components/photo_gallery/ShowStoryComponent.js
+++ src/components/photo_gallery/ShowStoryComponent.js
@@ -2,16 +2,20 @@
 
 import * as React from 'react';
 import { useState, useEffect } from 'react'
-import { Button, Grid } from '@mui/material';
+import { Button, Grid, } from '@mui/material';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import * as StoryContant from "./StoryConstant.js"
+import { Row, Container, Col } from 'react-bootstrap';
+import './style.css';
 
 export default function ShowImage() {
 
     const [images, setImages] = useState([])
     const [ngoAuth, setNgoAuth] = useState("")
     const navigate = useNavigate();
+    //const history = useHistory();
+
 
     useEffect(() => {
         // Update the document title using the browser API
@@ -38,40 +42,53 @@ export default function ShowImage() {
     const onAddClickHandler = () => {
         console.log('incdsc')
         navigate(`/addImage`);
+        //history.push('/addImage')
     };
     const onEditClickHandler = () => {
         navigate(`/editImage`);
+        //  history.push('/editImage')
     };
     return (
-        <Grid container justifyContent="center" alignItems={'center'} >
-            {
-                images.map((item, key) => (
-                    <div>
-                        <img
-                            style={{ width: '35%', height: '35%', margin: '20px' }}
-                            // style={width = '33.33%'; height= '33.33%';}
-                            src={item.image}
-                            // srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                            alt={item.title}
-                            loading="lazy"
-                        />
-                        <h3> {item.description} </h3>
-                        <br /><br />
-                    </div>
-                ))
-            }
-            <br /><br /><br />
+        // <Grid container justifyContent="center" alignItems={'center'} >
+        <Container>
+            <Row className='justify-content-center'>
+                <Col md={5}>
+                    {
+                        images.map((item, key) => (
+                            <Row>
+                                <img className='imageStyle'
+                                    // style={width = '33.33%'; height= '33.33%';}
+                                    src={item.image}
+                                    // srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                    alt={item.title}
+                                    loading="lazy"
+                                />
+                                <h3> {item.description} </h3>
+                            </Row>
+                        ))
+                    }
+                    {images && images.length === 0 &&
+                        <p className='text-danger' > <b>No stories added </b> </p>
+                    }
+                </Col>
+            </Row>
             {/* onClick={handleSubmit} */}
-            <Button variant="contained" component="span" margin='20px' onClick={onAddClickHandler}>
-                {/* <a href='http://localhost:3000/editImage'>Ngo Activity</a> */}
-                {/* https://gracious-givers-frontend-web.herokuapp.com/ frontend */}
-                Add Image
-            </Button><br /><br />
-            <Button variant="contained" component="span" margin='20px' onClick={onEditClickHandler}>
-                {/* <a href='http://localhost:3000/editImage'>Ngo Activity</a> */}
-                {/* https://gracious-givers-frontend-web.herokuapp.com/ frontend */}
-                Edit Image
-            </Button><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-        </Grid >
+            <Row>
+                <Col>
+                    <Button variant="contained" onClick={onAddClickHandler}>
+                        {/* <a href='http://localhost:3000/editImage'>Ngo Activity</a> */}
+                        {/* https://gracious-givers-frontend-web.herokuapp.com/ frontend */}
+                        Add Image
+                    </Button>
+                </Col>
+                <Col className='text-right'>
+                    <Button variant="contained" onClick={onEditClickHandler}>
+                        {/* <a href='http://localhost:3000/editImage'>Ngo Activity</a> */}
+                        {/* https://gracious-givers-frontend-web.herokuapp.com/ frontend */}
+                        Edit Image
+                    </Button>
+                </Col>
+            </Row>
+        </Container >
     )
 }
--- src/pages/photo_gallery/ShowStoryPage.js
+++ src/pages/photo_gallery/ShowStoryPage.js
@@ -2,6 +2,7 @@
 
 import Footer from "../../components/navbar/Footer";
 import Header from "../../components/navbar/Header";
+import React from "react";
 import ShowStoryComponent from "../../components/photo_gallery/ShowStoryComponent";
 import ShowStoryUserComponent from "../../components/photo_gallery/ShowStoryUserComponent";
 import * as StoryConstant from "../../components/photo_gallery/StoryConstant"
@@ -12,11 +13,12 @@ const ShowStoryPage = () => {
     const ngoAuth = "edf";
     // const navigate = useNavigate();
     return (
-        <div>
+        <React.Fragment>
             <Header />
             {ngoAuth.length > 0 ? <ShowStoryComponent /> : <ShowStoryUserComponent />}
             <Footer />
-        </div>
+            {/* </div> */}
+        </React.Fragment >
     );
 }
 
