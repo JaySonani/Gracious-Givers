2a1637650a15a895a5c07430d5dd389a9c90a897
viraj <viraj.shah@dal.ca>
2022-04-06 23:22:35 -0300

    added showimage component in fundraiser details

--- src/components/fundraiser/FundraiserDetails.js
+++ src/components/fundraiser/FundraiserDetails.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { Tabs, Tab } from 'react-bootstrap';
 import PictureGallery from './PictureGallery';
+import ShowStoryComponent from "../../components/photo_gallery/ShowStoryComponent";
 import './styles/fundraiserDetails.css';
 
 export default class FundraiserDetails extends React.Component {
@@ -16,24 +17,25 @@ export default class FundraiserDetails extends React.Component {
                     </div>
                 </div>
                 <div className='row'>
-                    <div className='col-12' style={{margin:'10px 0px', textAlign:'center'}}>
-                    <img src={event.image}
+                    <div className='col-12' style={{ margin: '10px 0px', textAlign: 'center' }}>
+                        <img src={event.image}
                             height="330rem"
                             width="560rem"
-                            className="img-fluid fundraiser-image rounded" 
+                            className="img-fluid fundraiser-image rounded"
                             alt={event.title} />
                     </div>
                 </div>
                 <div className='row'>
                     <div className='col-12' >
                         <div className='tab-div'>
-                            <Tabs defaultActiveKey="aboutFundraiser"                                  
-                                    className="mb-3">
+                            <Tabs defaultActiveKey="aboutFundraiser"
+                                className="mb-3">
                                 <Tab eventKey="aboutFundraiser" title="About Fundraiser" className="fundraiser-nav-link">
                                     <p className='tab-content'>{event.description}</p>
                                 </Tab>
                                 <Tab eventKey="updates" title="Updates">
-                                    <PictureGallery eventId={event.eventId} />
+                                    {/* <PictureGallery eventId={event.eventId} /> */}
+                                    <ShowStoryComponent />
                                 </Tab>
                             </Tabs>
                         </div>
