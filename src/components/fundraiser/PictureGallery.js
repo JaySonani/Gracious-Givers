0c3da8ba50cf24c35f67ed1b826c330951b51c46
viraj <viraj.shah@dal.ca>
2022-04-07 13:33:31 -0300

    final merge

--- src/components/fundraiser/PictureGallery.js
+++ src/components/fundraiser/PictureGallery.js
@@ -1,10 +1,25 @@
+// Author: Viraj Jigar Shah (B00879448)
+
+import ShowStoryComponent from "../../components/photo_gallery/ShowStoryComponent";
+import ShowStoryUserComponent from "../../components/photo_gallery/ShowStoryUserComponent";
+import * as StoryConstant from "../photo_gallery/StoryConstant"
+
 export default function PictureGallery(props) {
 
-    const eventId = props.eventId;
+    const ngoID = props.event.ngoId;
+    const event = props.event;
+    console.log(event)
+    //const id = event._id;
+
+    const ngoAuth = StoryConstant.getNgoId();
+    //console.log(ngoAuth + " Viraj ")
+    //console.log(event)
+
+    //console.log(ngoID)
     return (
         <>
-            This is the PictureGallery for event {eventId}
+            {/* This is the PictureGallery for event. */}
+            {ngoAuth === '' ? <ShowStoryUserComponent event={event} /> : <ShowStoryComponent event={event} />}
         </>
     );
-
 }
